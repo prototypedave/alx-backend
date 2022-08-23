4c77f6d907a7fa4b90ecf8ff995eab5c17a70576
prototypedave <popupgfj@gmail.com>
2022-08-23 13:58:51 +0000

    task 0

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python3
 """
-simple helper function that return a tuple of size two containing a 
-start index and an end index corresponding to the range of indexes to 
-return in a list for those particular pagination parameters
+simple helper function
 """
+
 
 def index_range(page, page_size):
     if page and page_size:

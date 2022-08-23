b67b7ac9f522d482b7a59f87fd7b4f07c46aad43
prototypedave <popupgfj@gmail.com>
2022-08-23 14:10:31 +0000

    fixed pycodestyle error on task 2

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -15,9 +15,7 @@ def index_range(page, page_size):
         return start_index, end_index
 
 class Server:
-    """
-    Server class to paginate a database of popular baby names.
-    """
+    """ Server class to paginate a database of popular baby names. """
     DATA_FILE = "Popular_Baby_Names.csv"
 
     def __init__(self):

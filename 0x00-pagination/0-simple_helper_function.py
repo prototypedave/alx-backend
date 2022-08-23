21e40d09a68a0339ee316e9651ac03219c09665f
prototypedave <popupgfj@gmail.com>
2022-08-23 14:02:53 +0000

    fixing documentation error

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -5,6 +5,7 @@ simple helper function
 
 
 def index_range(page, page_size):
+    """ returns a tuple of 2 containing a start index and end index """
     if page and page_size:
         start_index = (page - 1) * page_size
         end_index = start_index + page_size

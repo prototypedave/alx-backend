069f410d663b81aa40ed5a664a0be80f96dda82e
prototypedave <popupgfj@gmail.com>
2022-08-26 16:59:45 +0000

    corrections

--- 0x00-pagination/3-hypermedia_del_pagination.py
+++ 0x00-pagination/3-hypermedia_del_pagination.py
@@ -3,23 +3,24 @@
 Deletion-resilient hypermedia pagination
 """
 
+
 import csv
 import math
 from typing import List
 
 
 class Server:
-    """Server class to paginate a database of popular baby names.
-    """
+    """Server class to paginate a database of popular baby names """
     DATA_FILE = "Popular_Baby_Names.csv"
+
 
     def __init__(self):
         self.__dataset = None
         self.__indexed_dataset = None
 
+
     def dataset(self) -> List[List]:
-        """Cached dataset
-        """
+        """Cached dataset """
         if self.__dataset is None:
             with open(self.DATA_FILE) as f:
                 reader = csv.reader(f)
@@ -28,9 +29,9 @@ class Server:
 
         return self.__dataset
 
+
     def indexed_dataset(self) -> Dict[int, List]:
-        """Dataset indexed by sorting position, starting at 0
-        """
+        """Dataset indexed by sorting position, starting at 0 """
         if self.__indexed_dataset is None:
             dataset = self.dataset()
             truncated_dataset = dataset[:1000]
@@ -39,8 +40,9 @@ class Server:
             }
         return self.__indexed_dataset
 
+
     def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
-         """ return a dictionary """
+        """ return a dictionary """
         assert type(index) == int and type(page_size) == int
         assert 0 <= index < len(self.indexed_dataset())
         pages = []

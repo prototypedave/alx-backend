2d405141b99f1e9057c8d94515cae9977934fb52
prototypedave <popupgfj@gmail.com>
2022-08-30 21:31:44 +0000

    mandatory tasks

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""
+babel setup
+"""
+
+from flask import Flask, render_template
+from flask_babel import Babel
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config(object):
+    """ sets up default confugarions for babel """
+    
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+ 
+app.config.from_oject(Config)
+    """sets all the defaults in class config """
+
+
+@app.route("/")
+def get_template():
+    """ gets index file for the flask app """
+    return render_template('1-index.html')
+
+
+if __name__ == '__main__':
+    app.run()
---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+"""
+babel setup
+"""
+
+from flask import Flask, render_template
+from flask_babel import Babel, gettext
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config(object):
+    """ sets up default confugarions for babel """
+    
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+ 
+app.config.from_oject(Config)
+    """sets all the defaults in class config """
+
+
+@babel.localeselector
+def get_locale():
+    """ looks for the best language to use from the given objects """
+    request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/')
+def get_template():
+    """ gets index file for the flask app """
+    return render_template("3-index.html")
+
+
+if __name__ == '__main__':
+    app.run()
